9d687fdf03f01769eaebe05a895d6345a0b9b4e1
blibletype <maxymkoval2510@gmail.com>
2023-10-22 19:41:24 +0300

    added database.js to write and read data from file

---
+++ 02-cli-primitive-db/database.js
@@ -0,0 +1,39 @@
+import { readFile, writeFile } from 'fs/promises';
+
+const FILE_PATH = 'test.txt';
+
+/**
+ * Finds all users from the database.
+ * @returns {Promise<Array>} An array of user objects.
+ */
+export const findAll = async () => {
+  try {
+    const users = [];
+    const data = await readFile(FILE_PATH, { encoding: 'utf8' });
+
+    for (const row of data.split('\n')) {
+      if (!row) break;
+      users.push(JSON.parse(row));
+    }
+
+    return users;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/**
+ * Writes data to a file in JSON format.
+ * @param {Object} data - The data to be written to the file.
+ * @returns {Promise<void>} - A Promise that resolves when the data has been written to the file.
+ */
+export const create = async (data) => {
+  try {
+    await writeFile(FILE_PATH, JSON.stringify(data) + '\n', {
+      encoding: 'utf8',
+      flag: 'a',
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ 02-cli-primitive-db/index.js
@@ -0,0 +1,3 @@
+import inquirer from 'inquirer';
+
+await inquirer.prompt();
