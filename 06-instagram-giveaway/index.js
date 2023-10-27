ca585b222fca0a0d65418854d2ddf1e608898e59
blibletype <maxymkoval2510@gmail.com>
2023-10-27 19:21:53 +0300

    small adjustment

--- 06-instagram-giveaway/index.js
+++ 06-instagram-giveaway/index.js
@@ -1,9 +1,7 @@
 const { uniqueValues, existInAllFiles, existInAtleastTen } = require('./utils');
 
-(async () => {
-  console.time('Summary');
-  console.log('uniqueValues: ' + uniqueValues());
-  console.log('existInAllFiles: ' + existInAllFiles());
-  console.log('existInAtleastTen: ' + existInAtleastTen());
-  console.timeEnd('Summary');
-})();
+console.time('Summary');
+console.log('uniqueValues: ' + uniqueValues());
+console.log('existInAllFiles: ' + existInAllFiles());
+console.log('existInAtleastTen: ' + existInAtleastTen());
+console.timeEnd('Summary');
